ffe47c4a900666e2ded706da303fb9aad4a42656
chrkulbe <ckulbe@gmx.net>
2021-09-18 07:47:27 +0200

    ignore fd00 Unique Local Unicast adresses

--- dynv6.sh
+++ dynv6.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 while true
 do 
-	external_ipv6=`ip -6 a list "$INTERFACE" | grep "$GREP" | sed -r "s/.*inet6 (.*) scope .*/\1/"`
+	external_ipv6=`ip -6 a list "$INTERFACE" | grep -F "$GREP" | grep -Fv "inet6 fd00" | sed -r "s/.*inet6 (.*) scope .*/\1/"`
 	url="http://dynv6.com/api/update?hostname=$ZONE&token=$TOKEN&ipv6=$external_ipv6"
 	printf "request $url\n"
 	curl "$url"
